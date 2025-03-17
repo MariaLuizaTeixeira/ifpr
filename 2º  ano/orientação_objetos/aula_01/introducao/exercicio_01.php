8a8a92d3a4ab216846bf6956c47a39c8c349b427
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-03-17 11:33:21 -0300

    Update and rename 2º  ano/teste to 2º  ano/orientação_objetos/aula_01/introducao/exercicio_01.php

---
+++ 2º  ano/orientação_objetos/aula_01/introducao/exercicio_01.php
@@ -0,0 +1,29 @@
+<?php
+
+// while
+
+$a = 1;
+while ($a <= 10) 
+{
+    print($a . "\n");
+    $a++;
+}
+
+// do-while
+
+$a = 1;
+
+do 
+{
+    print($a . "\n");
+    $a++;
+} while ($a <= 10);
+
+// for
+
+for ($i=1; $i <= 10; $i++) 
+{ 
+    print($i . "\n");
+}
+
+?>
