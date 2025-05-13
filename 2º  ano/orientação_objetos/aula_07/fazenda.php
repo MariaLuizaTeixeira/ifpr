fdb0c4972d918d1ff1eb9f4cf7bd09c0ab588500
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2025-05-13 11:47:45 -0300

    Update fazenda.php

--- 2º  ano/orientação_objetos/aula_07/fazenda.php
+++ 2º  ano/orientação_objetos/aula_07/fazenda.php
@@ -5,11 +5,29 @@ class Animal {
     public $NumeroPatas;
 
     function emitirSom() {
-        if($this->especie == "Cachorro") {
-            print("Au Au Au!\n");
+        if($this->especie == "Ema") {
+            print("Bu-úp!\n");
         }
         elseif ($this->especie = "Porco") {
             print("Hoinc Hoinc!\n");
+        }
+        elseif ($this->especie = "Cobra") {
+            print("Ssssss...");
+        }
+        elseif ($this->especie = "Baleia") {
+            print("Oooo-oooo!");
+        }
+        elseif ($this->especie = "Aranha") {
+            print("Shhhk-shhk!");
+        }
+    }
+
+    function Andar() {
+        if($this->NumeroPatas == 2 || $this->NumeroPatas == 4 || $this->NumeroPatas == 6) {
+            print("Esse animal anda.\n");
+        }
+        elseif ($this->NumeroPatas = 0) {
+            print("Esse animal não anda.\n");
         }
     }
 }
