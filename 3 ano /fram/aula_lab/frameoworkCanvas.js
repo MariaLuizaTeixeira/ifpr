8ca948ef79f94f57238a2e73bb9788f0aa14382a
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2026-03-27 09:08:44 -0300

    Update frameoworkCanvas.js

--- 3 ano /fram/aula_lab/frameoworkCanvas.js
+++ 3 ano /fram/aula_lab/frameoworkCanvas.js
@@ -2,17 +2,22 @@ const body = document.getElementsByTagName("body")[0];
 const canvas = document.createElement("canvas");
 body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
+const arco = document.querySelector("circulo")
 
 const circulo = {
   x: 100,
   y: 100,
   r: 50,
-  desenhar:function () {
+  cor: "orange",
+  desenhar:function (raio) {
     ctx.beginPath();
-    ctx.arc(thhis.x, this.y, this.r, 0, Math.PI * 2);
-    ctx.fillStyle="orange";
+    ctx.arc(thhis.x, this.y, raio, 0, Math.PI * 2);
+    ctx.fillStyle = this.cor;
     ctx.closePath();
   }
 };
 
-if(document.querySelector("circulo")) circulo.desenhar();
+if(arco) {
+  let raio = arco.getAttribute("raio");
+  circulo.desenhar(raio);
+}
