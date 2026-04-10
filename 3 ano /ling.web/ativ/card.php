9fc49b5a2f5777337b385b559c3a53cd868f6096
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2026-04-10 14:00:04 -0300

    Update card.php to display movie details dynamically
    Refactor card display logic to use dynamic movie data.

--- 3 ano /ling.web/ativ/card.php
+++ 3 ano /ling.web/ativ/card.php
@@ -3,11 +3,25 @@
 require_once("modelo/Filme.php");
 
 $filme = new Filme();
-$filme->setNome($_POST['nome']); 
-$filme->setNota($_POST['nota']);
-$filme->setUrl_imagem($_POST['url']); 
-$filme->setGenero($_POST['genero']);
+$filme->setNome($_POST['nome']); $filme->setNota($_POST['nota']);
+$filme->setUrl_imagem($_POST['url']); $filme->setGenero($_POST['genero']);
 $filme->setDecada($_POST['decada']); 
+print(
+"<div class='d-flex justify-content-center align-items-center vh-100'>
+    <div class='card' style='width: 18rem'>
+      <img src='" . $filme->getUrl_imagem() . "' class='card-img-top' alt='...' />
+      <div class='card-body'>
+        <h5 class='card-title'>" . $filme->getNome() ."</h5>
+        <p class='card-text'>
+          " . $filme->getNome() . " é um filme de " . $filme->getGenero() . " dos
+          anos " . $filme->converterNumeroParaDecada($filme->getDecada()) . ".
+          Você deu " . $filme->getNota() . " estrelas para esse filme.
+        </p>
+        <a href='formulario.php' class='btn btn-primary'>Inserir novo filme</a>
+      </div>
+    </div>
+  </div>"
+); 
 ?>
 
 <!doctype html>
@@ -21,19 +35,7 @@ $filme->setDecada($_POST['decada']);
       rel="stylesheet"
     />
   </head>
-
   <body>
-    <div class="card" style="width: 18rem">
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
